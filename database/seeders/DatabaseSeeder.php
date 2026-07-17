09d8923ca211abd8fd31a7e5c0f677135b1e8901
RezkiPlay <muhammadrezky006@gmail.com>
2026-07-18 05:21:42 +0800

    Pertemuan 14: Setting & Login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders\SettingSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-      $this->call([
-
-      UserSeeder::class,
-      ]);
+        $this->call([
+            UserSeeder::class,
+            SettingSeeder::class,
+        ]);
     }
 }
